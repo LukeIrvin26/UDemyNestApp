7256aa994541cfbdf03b1e67e8f97ae08a44e3c3
Luke Irvin <thelukester145@gmail.com>
2021-09-20 17:44:31 -0500

    Up to validation

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
+
+@Controller()
+export class AuthController {
+  constructor(private userService: UserService) {}
+  @Post('register')
+  async register(@Body() body) {
+    const hashed = await bcrypt.hash(body.password, 12);
+    return this.userService.create({
+      first_name: body.first_name,
+      last_name: body.last_name,
+      email: body.email,
+      password: hashed,
+    });
+  }
+}
